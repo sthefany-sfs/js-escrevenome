fdc277f33af04eb5afafdea67ee5f3376951b503
sthefany-sfs <146599981+sthefany-sfs@users.noreply.github.com>
2023-10-21 20:25:40 -0300

    atualizando arquivo js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
 
 function setup() {
 
-createCanvas(600, 600);
-
-background("black");
-
-}
-
-function draw() {
-
-stroke("blue");
-
-fill("red");
-
-if(mouseIsPressed){
-
-rect(mouseX, mouseY, 20, 35);
-
-}
-
-}
+    createCanvas(600, 600);
+    
+    background("black");
+    
+    }
+    
+    function draw() {
+    
+    stroke("blue");
+    
+    fill("red");
+    
+    if(mouseIsPressed){
+    
+    rect(mouseX, mouseY, 20, 35);
+    
+    }
+    
+    }
